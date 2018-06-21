cc27099e2d5b03100a83fd0463fb71399e0d25f8
Alex Birch <ABirch@scottlogic.com>
2018-06-21 10:52:13 +0100

    button, possibly hooked up to Redux

---
+++ kill-node.ps1
@@ -0,0 +1,2 @@
+#!/usr/bin/env pwsh
+Get-Process -Name node | Stop-Process
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
+import {connect, Dispatch} from 'react-redux'
+import {IncreaseNumberAction} from "./actions/piano";
 import './App.css';
 import Keyboard from "./Keyboard";
 
-import logo from './logo.svg';
-
-// interface IProps {}
+interface IMappedProps {
+  increaseNumber: () => void;
+}
+interface IProps extends IMappedProps {}
 
 interface IState {
   scaleDegree: number;
 }
 
-class App extends React.Component<{}, IState> {
-  constructor(props:{}) {
+class App extends React.Component<IProps, IState> {
+  constructor(props:IProps) {
     super(props);
+    // console.debug(props.dispatch)
 
     this.state = {
       // populate state fields according to props fields
@@ -21,19 +25,23 @@ class App extends React.Component<{}, IState> {
   }
 
   public render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <Keyboard scaleLength={7}/>
+        <input type="button" onClick={this.props.increaseNumber}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch:Dispatch):IMappedProps => {
+  return {
+    increaseNumber: () => dispatch(new IncreaseNumberAction({amount:1}))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
