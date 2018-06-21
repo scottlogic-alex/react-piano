ef9e4c3b9428219c13e5669b2e9df4686868b3f7
Alex Birch <ABirch@scottlogic.com>
2018-06-21 09:12:08 +0100

    remove unused

--- src/actions/Action.ts
+++ src/actions/Action.ts
@@ -11,16 +11,11 @@ export class Action<Payload> implements AnyAction{
   // @ts-ignore
   public readonly type: string;
   // @ts-ignore
-  private readonly payload: Payload; // stub; needed for Is() method's type-inference to work, for some reason
+  private readonly payload: Payload; // stub; needed for IsType() method's type-inference to work, for some reason
   constructor(payload: Payload) {
     // @ts-ignore
     this.type = this.constructor.name;
     // this.payload = payload;
     Object.assign(this, payload);
-  }
-
-  public Is<Payload2>(actionType: new(..._:any[])=>Action<Payload2>): this is Payload2 {
-    return this.type === actionType.name;
-    // return this instanceof actionType; // alternative
   }
 }
