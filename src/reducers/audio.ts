18af69cc9aa15e5277b019e1e257ab2a6c96f724
Alex Birch <ABirch@scottlogic.com>
2018-06-25 10:33:28 +0100

    add audio context, oscillators

--- src/reducers/audio.ts
+++ src/reducers/audio.ts
@@ -1,9 +1,10 @@
 import {Action, Reducer} from "redux";
 import {Action as MyAction} from "../actions/Action";
 import {AddVoiceAction, RemoveVoiceAction} from "../actions";
+import * as _ from "lodash";
 
 export interface IVoice {
-  oscillator: OscillatorType
+  oscillator: OscillatorNode
 }
 
 export interface IVoiceState {
@@ -16,12 +17,16 @@ const initialState:IVoiceState = {
   context: new ((window as any).AudioContext || (window as any).webkitAudioContext)()
 }
 
+const newVoice = (context: AudioContext):IVoice => {
+  return { oscillator: context.createOscillator() };
+}
+
 const audio:Reducer<IVoiceState, Action<any>> = (state = initialState, action:Action<any>) => {
   if (MyAction.IsType(action, AddVoiceAction)) {
-    return {...state}
+    return {...state, voices: {...state.voices, [action.id]: newVoice(state.context) }}
   }
   if (MyAction.IsType(action, RemoveVoiceAction)) {
-    return {...state}
+    return {...state, voices: _.omit(state.voices, action.id)}
   }
   return state
 }
